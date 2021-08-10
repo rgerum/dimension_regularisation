ab02b7ce4a6f3c8806f2242aa24d0707e28fde91
rgerum <14153051+rgerum@users.noreply.github.com>
2021-08-10 15:03:10 -0400

    added exp_diff_strength.py name_fill

--- exp_diff_strength.py
+++ exp_diff_strength.py
@@ -6,8 +6,8 @@ for i in range(3):
     for strength in [0, 0.001, 0.01, 0.1, 1]:
         if strength == 0:
             reg = 0
-            os.system(f"python sbatch.py 1 exp_diff_strength run_conv_augmentation2.py --reg1 {reg} --reg2 {reg} --reg3 {reg} --reg4{reg} --reg5{reg}")
+            os.system(f"python sbatch.py 1 exp_diff_strength_{i}_{reg} run_conv_augmentation2.py --reg1 {reg} --reg2 {reg} --reg3 {reg} --reg4{reg} --reg5{reg}")
         else:
             for sig in [1, -1]:
                 reg = sig*strength
-                os.system(f"python sbatch.py 1 exp_diff_strength run_conv_augmentation2.py --reg1 {reg} --reg2 {reg} --reg3 {reg} --reg4{reg} --reg5{reg}")
+                os.system(f"python sbatch.py 1 exp_diff_strength_{i}_{reg} run_conv_augmentation2.py --reg1 {reg} --reg2 {reg} --reg3 {reg} --reg4{reg} --reg5{reg}")
