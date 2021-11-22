5663919e3e242684a9212ca94eb5c94a4494dc54
rgerum <14153051+rgerum@users.noreply.github.com>
2021-11-22 11:09:26 -0500

    fixed params in exp_comp_reg_methods.py

--- exp_comp_reg_methods.py
+++ exp_comp_reg_methods.py
@@ -4,5 +4,5 @@ from pathlib import Path
 
 for i in range(3):
     for reg in [0, 0.0001, 0.001, 0.01, 0.1, 1, 10]:
-        os.system(f"python sbatch.py 1 gamma_{i}_{reg} run_conv_comp.py --reg1 {reg} --reg1value{reg} --reg_type=gamma --output comp/reg_gamma_{i}_{reg}")
-        os.system(f"python sbatch.py 1 fit_{i}_{reg} run_conv_comp.py --reg1 {reg} --reg1value{reg} --reg_type=fit --output comp/reg_fit_{i}_{reg}")
+        #os.system(f"python sbatch.py 1 gamma_{i}_{reg} run_conv_comp.py --reg1 {reg} --reg1value{reg} --reg_type gamma --output comp/reg_gamma_{i}_{reg}")
+        os.system(f"python sbatch.py 1 fit2_{i}_{reg} run_conv_comp.py --reg1 {reg} --reg1value{reg} --reg_type fit --output comp/reg_fit2_{i}_{reg}")
--- sync2.sh
+++ sync2.sh
@@ -1,3 +1,3 @@
-rsync -rzvhh rgerum@cedar.computecanada.ca:~/scratch/dimension_regularisation/logs/* cedar_logs --exclude='*alpha2.csv'
+rsync -rzvhh rgerum@cedar.computecanada.ca:~/scratch/dimension_regularisation/dimension_regularisation/logs/* cedar_logs --exclude='*alpha2.csv'
 #find . -depth -type d -empty -delete
 #ssh rgerum@cedar.computecanada.ca find /home/rgerum/scratch/dimension_regularisation/logs -depth -type d -empty -not -path /home/rgerum/scratch/dimension_regularisation/logs -delete
