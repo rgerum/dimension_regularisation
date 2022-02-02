85a3a0ea3a794a8dbb42b12ebe8f1dafa51c3cf8
rgerum <14153051+rgerum@users.noreply.github.com>
2022-02-02 15:40:22 -0500

    jobs.csv for reg1 = 0

--- exp_cifar10_corrupted.py
+++ exp_cifar10_corrupted.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 data = []
 for i in range(3):
-    for strength in [0.001, 0.01, 0.1, 1]:
+    for strength in [0]:#0.001, 0.01, 0.1, 1]:
         for reg1value in [0.6, 0.8, 1.0, 1.2, 1.4]:
             reg = strength
             data.append(dict(iter=i, reg1=strength, reg1value=reg1value, output=f"expcifar/iter-{i}_reg1-{strength}_reg1value-{reg1value}"))
