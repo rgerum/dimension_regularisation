968c80c9b696c7caa00d149336fd32c59218be11
rgerum <14153051+rgerum@users.noreply.github.com>
2022-04-12 16:56:12 -0400

    pca_variance.py add get_eigen_vectors

--- dimension_regularisation/pca_variance.py
+++ dimension_regularisation/pca_variance.py
@@ -36,3 +36,31 @@ def linear_fit(x_data, y_data):
     b = tf.reduce_sum((x_data - x_mean) * (y_data - y_mean)) / tf.reduce_sum((x_data - x_mean) ** 2)
     a = y_mean - (b * x_mean)
     return a, b
+
+
+@tf.function
+def get_eigen_vectors(data):
+    """ calculate the eigenvalues of the covariance matrix """
+    normalized_data = data - tf.reduce_mean(data, axis=0)[None]
+    # Finding the Eigen Values and Vectors for the data
+    sigma = tf.tensordot(tf.transpose(normalized_data), normalized_data, axes=1)
+    eigen_values, eigen_vectors = tf.linalg.eigh(sigma)
+
+    # return the eigenvectors
+    return eigen_vectors
+
+
+@tf.function
+def get_eigen_values_from_vectors(data, eigen_vectors):
+    # get the normalized covariance matrix
+    normalized_data = data - tf.reduce_mean(data, axis=0)[None]
+    sigma = tf.tensordot(tf.transpose(normalized_data), normalized_data, axes=1)
+    # calculate the eigenvector matrix
+    v = tf.matmul(eigen_vectors, tf.matmul(sigma, eigen_vectors), True, False)
+    # get the diagonal
+    v2 = tf.linalg.diag_part(v)
+    # normalize it
+    v3 = v2 / tf.reduce_sum(v2)
+    # sort it
+    v4 = tf.sort(v3, direction='DESCENDING')
+    return v4
