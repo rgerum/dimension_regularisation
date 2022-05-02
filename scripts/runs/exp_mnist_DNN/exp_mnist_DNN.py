1c7ce105fb7710b501088f7b8a3047872aee6efe
rgerum <14153051+rgerum@users.noreply.github.com>
2022-05-02 16:38:59 -0400

    extended exp_mnist_DNN.py to more alpha values

--- scripts/runs/exp_mnist_DNN/exp_mnist_DNN.py
+++ scripts/runs/exp_mnist_DNN/exp_mnist_DNN.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 data = []
 for i in range(3):
-    for gamma in [True, False]:
+    for gamma in [False]:
         for strength in [0, 0.001, 0.01, 0.1, 1]:
             #vs = [1.6, 1.8, 2.0, 2.2, 2.4]
             #if gamma is False:
-            vs = [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4]
+            #vs = [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4]
+            vs = [2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0]
             for reg1value in vs:
                 reg = strength
                 data.append(dict(iter=i, gamma=gamma, reg_strength=strength, reg_target=reg1value, output=f"../../../results/expcifar5_DNN_extened_range/iter-{i}_gamma-{gamma}_reg1-{strength}_reg1value-{reg1value}"))
