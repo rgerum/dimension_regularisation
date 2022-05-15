4477bb2018e82ea7cfa070909c3f7631286da253
rgerum <14153051+rgerum@users.noreply.github.com>
2022-05-15 09:40:12 -0400

    changed xmax to 1000

--- scripts/runs/exp_cnn/exp.py
+++ scripts/runs/exp_cnn/exp.py
@@ -19,7 +19,7 @@ for i in range(3):
                 for reg1value in vs:
                     reg = strength
                     data.append(dict(iter=i, gamma=gamma, dataset=dataset, reg_strength=strength, reg_target=reg1value,
-                                     output=f"../../../results/exp_cnn/iter-{i}_dataset-{dataset}_gamma-{gamma}_reg1-{strength}_reg1value-{reg1value}"))
+                                     output=f"../../../results/exp_cnn_maxx1k/iter-{i}_dataset-{dataset}_gamma-{gamma}_reg1-{strength}_reg1value-{reg1value}"))
 data = pd.DataFrame(data)
 print(data)
 data.to_csv("jobs.csv")
