69c5824b5cbcf4493d841eaf9d85f6a3cb3a1e38
rgerum <14153051+rgerum@users.noreply.github.com>
2022-06-07 16:55:31 -0400

    added different dense1 values for exp_mnist_DNN.py

--- scripts/runs/exp_mnist_DNN/exp_mnist_DNN.py
+++ scripts/runs/exp_mnist_DNN/exp_mnist_DNN.py
@@ -7,18 +7,19 @@ import numpy as np
 
 data = []
 for i in range(3):
-    for gamma in [False]:
-        #for strength in [0, 0.001, 0.01, 0.1, 1]:
-        for strength in [0, 0.1, 1]:
-            #vs = [1.6, 1.8, 2.0, 2.2, 2.4]
-            #if gamma is False:
-            #vs = [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4]
-            #vs = [2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0]
-            vs = np.arange(0.6, 4.1, 0.2)
-            for reg1value in vs:
-                reg = strength
-                data.append(dict(iter=i, gamma=gamma, dataset="cifar10", reg_strength=strength, reg_target=reg1value,
-                                 output=f"../../../results/expcifar5_DNN_extened_range_cifar10/iter-{i}_dataset-cifar10_gamma-{gamma}_reg1-{strength}_reg1value-{reg1value}"))
+    for dense1 in [20_000, 200_000]:
+        for gamma in [False]:
+            #for strength in [0, 0.001, 0.01, 0.1, 1]:
+            for strength in [0, 0.1, 1]:
+                #vs = [1.6, 1.8, 2.0, 2.2, 2.4]
+                #if gamma is False:
+                #vs = [0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4]
+                #vs = [2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0]
+                vs = np.arange(0.6, 4.1, 0.2)
+                for reg1value in vs:
+                    reg = strength
+                    data.append(dict(iter=i, dense1=dense1, gamma=gamma, dataset="cifar10", reg_strength=strength, reg_target=reg1value,
+                                     output=f"../../../results/shallow_dense_deep/iter-{i}_dense1-{dense1}_gamma-{gamma}_reg1-{strength}_reg1value-{reg1value}"))
 data = pd.DataFrame(data)
 print(data)
 data.to_csv("jobs.csv")
